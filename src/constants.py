d27e201583f829eff03463020c2e6219a6dcb567
Alexander <sasha.sokolovskiy99gmail.com>
2020-04-17 12:36:41 +0300

    add constants and requirements

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+
+START_URL = "https://www.amazon.co.uk/AMD-Ryzen-3900X-Processor-Cache/dp/B07SXMZLP9/"  # 4 offer-list pages
+# START_URL = 'https://www.amazon.co.uk/Sony-MDRZX310-Foldable-Headphones/dp/B00I3LUWQA/'  # 2 offer-list pages
+
+
+START_URL_REGEX = r"^(?P<domain>.+?\/\/.+?|.+?)\/(?P<product_name>.+?)\/dp\/(?P<asin>[A-Z0-9]{10})"  # domain, product_name, asin
+SELLER_URL_REGEX = r"seller\=(?P<seller_id>[A-Z0-9]+)"  # seller_id
+
+# template use OFFER_LISTING_URL_TEMPLATE.format(domain, asin, page_num, offset)
+# offset = (1 - page_num) * ITEM_PER_PAGE
+OFFER_LISTING_URL_TEMPLATE = "{}/gp/offer-listing/{}/ref=olp_page_{}?ie=UTF8&startIndex={}"
+
+# template use PRODUCT_URL_TEMPLATE.format(domain, asin, seller)
+PRODUCT_URL_TEMPLATE = "{}/dp/{}?m={}\n"
+
+ITEM_PER_PAGE = 10
